690ff6681ca4416b157639c0a4f1a3395cb16f68
mykhailoponomarenko <ponomarenko.pn@ucu.edu.ua>
2024-10-04 12:01:28 +0300

    Fixed random function

--- src/main/java/lotr/Noble.java
+++ src/main/java/lotr/Noble.java
@@ -1,16 +1,15 @@
 package lotr;
 
-
 import java.util.Random;
 
 public class Noble extends Character {
     public Noble(int num) {
-super(new Random().nextInt(num, num + 11), new Random().nextInt(num, num + 11), new NobleKick());
+        super(new Random().nextInt(11) + num, new Random().nextInt(11) + num, new NobleKick());
         // Random rand = new Random();
         // int a = rand.nextInt(11);
         // this.hp = num + a;
         // int b = rand.nextInt(11);
         // this.power = num + b;
     }
-    
+
 }
